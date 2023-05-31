2a0d8593d173a9a50c63bc59d4be283919018b66
Christopher <christopher@techelevator.com>
2023-05-31 09:18:01 -0400

    updates

---
+++ module-1/12_Polymorphism/lecture/src/main/java/com/techelevator/avengers/Hulk.java
@@ -0,0 +1,13 @@
+package com.techelevator.avengers;
+
+public class Hulk extends Avenger{
+
+    public Hulk(String name){
+        super(name, "Super Strength");
+    }
+
+    public void smash(){
+        System.out.println("Hulk Smash!");
+    }
+
+}
