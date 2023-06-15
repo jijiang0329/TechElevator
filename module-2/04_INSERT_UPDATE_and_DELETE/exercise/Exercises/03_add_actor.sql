c5cbd9d04e89b292d764dd55de943a9b321ad0eb
Jingtong Jiang <jacqueline0329@hotmail.com>
2023-06-15 12:41:19 -0700

    Submitting daily exercise Week5 Day4

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/03_add_actor.sql
@@ -1,2 +1,10 @@
 -- 3. Did you know Eric Stoltz was originally cast as Marty McFly in "Back to the Future"? Add Eric Stoltz to the list of actors for "Back to the Future" (1 row)
-
+insert into movie_actor
+values (
+(select movie_id
+from movie 
+where title = 'Back to the Future'),
+(select person_id
+from person 
+where person_name = 'Eric Stoltz')
+)
