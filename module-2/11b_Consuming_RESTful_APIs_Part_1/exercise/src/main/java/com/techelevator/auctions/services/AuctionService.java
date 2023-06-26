520d7d5938a56b2a794396b15a194c19a7cd4ed6
Jingtong Jiang <jacqueline0329@hotmail.com>
2023-06-26 12:36:23 -0700

    Submitting daily exercise Week7 Day1

--- module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ module-2/11b_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,33 +1,39 @@
 package com.techelevator.auctions.services;
 
+import org.apiguardian.api.API;
 import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.auctions.model.Auction;
 
 public class AuctionService {
 
-    public static String API_BASE_URL = "http://localhost:3000/auctions/";
+    public static String API_BASE_URL = "http://localhost:3000/auctions";
     private RestTemplate restTemplate = new RestTemplate();
 
 
     public Auction[] getAllAuctions() {
         // call api here
-        return null;
+        Auction[] auctions = restTemplate.getForObject(API_BASE_URL, Auction[].class);
+        return auctions;
     }
 
     public Auction getAuction(int id) {
         // call api here
-        return null;
+        Auction auction = restTemplate.getForObject(API_BASE_URL + "/" +id, Auction.class);
+        return auction;
+
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
         // call api here
-        return null;
+        Auction[] auctions = restTemplate.getForObject(API_BASE_URL + "?title=" +title, Auction[].class);
+        return auctions;
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
         // call api here
-        return null;
+        Auction[] auctions = restTemplate.getForObject(API_BASE_URL + "?currentBid_lte=" +price, Auction[].class);
+        return auctions;
     }
 
 }
