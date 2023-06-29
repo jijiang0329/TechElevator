a0738bf1d926eb055ac9bcf74cc9cfed48c0c7e3
Jingtong Jiang <jacqueline0329@hotmail.com>
2023-06-29 11:26:59 -0700

    Submitting daily exercise Week7 Day4

--- module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,6 +2,7 @@ package com.techelevator.auctions.controller;
 
 import java.util.List;
 
+import com.techelevator.auctions.exception.DaoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auctions")
@@ -38,6 +41,7 @@ public class AuctionController {
         return auctionDao.getAuctions();
     }
 
+
     @RequestMapping(path = "/{id}", method = RequestMethod.GET)
     public Auction get(@PathVariable int id) {
         Auction auction = auctionDao.getAuctionById(id);
@@ -48,10 +52,26 @@ public class AuctionController {
         }
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public Auction create(@RequestBody Auction auction) {
+    public Auction create(@Valid @RequestBody Auction auction) {
+
         return auctionDao.createAuction(auction);
     }
 
+    @RequestMapping(path="/{id}", method=RequestMethod.PUT)
+    public Auction updateReservation(@PathVariable int id,@Valid @RequestBody Auction acution){
+        try{
+            acution.setId(id);
+            return auctionDao.updateAuction(acution);
+        }catch(DaoException ex){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "We could find the reservation");
+        }
+    }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path="/{id}", method=RequestMethod.DELETE)
+    public void deleteReservation(@PathVariable int id){
+        auctionDao.deleteAuctionById(id);
+    }
 }
