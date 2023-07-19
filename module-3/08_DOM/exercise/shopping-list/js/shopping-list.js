73cedd32c952c32fedab80b58320d351a4d41aa5
Jingtong Jiang <jacqueline0329@hotmail.com>
2023-07-19 11:21:05 -0700

    Submitting daily exercise Week10 Day3

--- module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
@@ -1,23 +1,42 @@
 // add pageTitle
-
+const pageTitle = "My Shopping List";
 // add groceries
-
+const groceries = ["Eggs", "Half&Half", "Peaches", "Butter", "Chips", "Soda", "Ice Cream", "Cookies", "Onions", "Tomatoes"]
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const h1 = document.getElementById("title");
+  h1.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const ul = document.getElementById("groceries");
+  groceries.forEach((grocery) => {
+    const li = document.createElement('li');
+    li.innerText = grocery;
+    ul.appendChild(li);
+  });
+  
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  const ul = document.getElementById("groceries");
+  groceries.forEach((grocery) => {
+    const li = document.createElement('li');
+    li.setAttribute("class", "completed")
+    li.innerText = grocery;
+    ul.appendChild(li);
+  });
+}
 
 setPageTitle();
 
