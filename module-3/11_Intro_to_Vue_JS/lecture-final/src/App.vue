c215371ab77ec6d12a482314bcf9c9bad80bf8d9
Christopher <test>
2023-07-24 12:49:41 -0400

    homework and lecture

---
+++ module-3/11_Intro_to_Vue_JS/lecture-final/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    
+    <product-reviews />
+    <p>Test Paragraph</p>
+  </div>
+</template>
+
+<script>
+import ProductReviews from "./components/ProductReviews.vue"
+
+export default {
+  name: "app",
+  components: {
+    ProductReviews
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  max-width: 800px;
+}
+</style>
