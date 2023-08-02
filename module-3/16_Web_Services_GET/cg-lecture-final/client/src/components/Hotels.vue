6f7a4c31b2e9ab9fa0a4a6b0190e1166ef26c649
Christopher <test>
2023-08-02 12:47:36 -0400

    lecture and hw

---
+++ module-3/16_Web_Services_GET/cg-lecture-final/client/src/components/Hotels.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <h1>Hotels</h1>
+
+      <img v-if="isLoading" src="../assets/ping_pong_loader.gif" />
+
+      <div v-if="!isLoading">
+        <h2  v-for="hotel in hotels" v-bind:key="hotel.id">
+            {{hotel.name}}
+        </h2>
+      </div>
+  </div>
+</template>
+
+<script>
+import HotelService from '../services/HotelService'
+
+export default {
+    data(){
+        return {
+            isLoading: true,
+            hotels: []
+        }
+    },
+    created(){
+        HotelService.getHotels().then( (response) => {
+            this.hotels = response.data;
+            console.log(response.data);
+            this.isLoading = false;
+        })
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
